352e092af81dfcc364f1f30bbd32ac3d26cccabd
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-10 03:41:14 -0300

    feat: alterando query's do backend

--- Backend/index.js
+++ Backend/index.js
@@ -37,32 +37,34 @@ pool.query(`
         console.error('Erro ao criar tabela empresas:', err);
     } else {
         console.log('Tabela empresas criada com sucesso');
-    }
-});
 
-pool.query(`
-    CREATE TABLE IF NOT EXISTS estagiarios (
-        id SERIAL PRIMARY KEY,
-        nome TEXT NOT NULL,
-        cpf TEXT NOT NULL,
-        email TEXT NOT NULL,
-        telefone TEXT NOT NULL,
-        curso TEXT NOT NULL,
-        periodo TEXT NOT NULL,
-        instituicao TEXT NOT NULL,
-        cargaHoraria TEXT NOT NULL
-    )
-`, (err, result) => {
-    if (err) {
-        console.error('Erro ao criar tabela estagiarios:', err);
-    } else {
-        console.log('Tabela estagiarios criada com sucesso');
+        pool.query(`
+            CREATE TABLE IF NOT EXISTS estagiarios (
+                id SERIAL PRIMARY KEY,
+                nome TEXT NOT NULL,
+                cpf TEXT NOT NULL,
+                email TEXT NOT NULL,
+                telefone TEXT NOT NULL,
+                curso TEXT NOT NULL,
+                periodo TEXT NOT NULL,
+                instituicao TEXT NOT NULL,
+                carga_horaria TEXT NOT NULL,
+                empresa_id INTEGER REFERENCES empresas(id)
+            )
+        `, (err, result) => {
+            if (err) {
+                console.error('Erro ao criar tabela estagiarios:', err);
+            } else {
+                console.log('Tabela estagiarios criada com sucesso');
+            }
+        });
+
     }
 });
 
 app.post('/cadastro', (req, res) => {
     const { nome, cnpj, email, senha } = req.body;
-    pool.query('INSERT INTO projetos (nome, cnpj, email, senha) VALUES ($1, $2, $3, $4)', [nome, cnpj, email, senha ], (err, result) => {
+    pool.query('INSERT INTO empresas (nome, cnpj, email, senha) VALUES ($1, $2, $3, $4)', [nome, cnpj, email, senha ], (err, result) => {
         if (err) {
             res.status(500).send(err.toString());
         } else {
@@ -71,11 +73,22 @@ app.post('/cadastro', (req, res) => {
     });
 });
 
+app.get('/empresas/:id', (req, res) => {
+    const id = req.params.id;
+    pool.query('SELECT nome, cnpj, email FROM empresas WHERE id = $1', [id], (err, result) => {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.json(result.rows[0]);
+        }
+    });
+});
+
 
 app.post('/login', (req, res) => {
     const { email, senha } = req.body;
   
-    pool.query('SELECT id, nome, email FROM projetos WHERE email = $1 AND senha = $2', [email, senha], (err, result) => {
+    pool.query('SELECT id, nome, email FROM empresas WHERE email = $1 AND senha = $2', [email, senha], (err, result) => {
       if (err) {
         res.status(500).send(err.toString());
       } else if (result.rows.length === 0) {
@@ -90,8 +103,8 @@ app.post('/login', (req, res) => {
 
 
     app.post('/estagiarios', (req, res) => {
-        const { nome, cpf, email, telefone, curso, periodo, instituicao, cargaHoraria } = req.body;
-        pool.query('INSERT INTO estagiarios (nome, cpf, email, telefone, curso, periodo, instituicao, cargaHoraria) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [nome, cpf, email, telefone, curso, periodo, instituicao, cargaHoraria ], (err, result) => {
+        const { nome, cpf, email, telefone, curso, periodo, instituicao, carga_horaria, empresa_id } = req.body;
+        pool.query('INSERT INTO estagiarios (nome, cpf, email, telefone, curso, periodo, instituicao, carga_horaria, empresa_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)', [nome, cpf, email, telefone, curso, periodo, instituicao, carga_horaria, empresa_id], (err, result) => {
             if (err) {
                 res.status(500).send(err.toString());
             } else {
@@ -123,8 +136,8 @@ app.post('/login', (req, res) => {
 
     app.put('/estagiarios/:id', (req, res) => {
         const id = req.params.id;
-        const { nome, cpf, email, telefone, curso, periodo, instituicao, cargaHoraria } = req.body;
-        pool.query('UPDATE estagiarios SET nome = $1, cpf = $2, email = $3, telefone = $4, curso = $5, periodo = $6, instituicao = $7, cargaHoraria = $8 WHERE id = $9', [nome, cpf, email, telefone, curso, periodo, instituicao, cargaHoraria, id], (err, result) => {
+        const { nome, cpf, email, telefone, curso, periodo, instituicao, carga_horaria } = req.body;
+        pool.query('UPDATE estagiarios SET nome = $1, cpf = $2, email = $3, telefone = $4, curso = $5, periodo = $6, instituicao = $7, carga_horaria = $8 WHERE id = $9', [nome, cpf, email, telefone, curso, periodo, instituicao, carga_horaria, id], (err, result) => {
             if (err) {
                 res.status(500).send(err.toString());
             } else {
