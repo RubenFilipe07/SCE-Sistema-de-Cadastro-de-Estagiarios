deebadd829909c58c2ce20c7580b7083022bd9d6
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-10 04:32:49 -0300

    feat: alterando rotas autenticadas

--- Aplicação/SCE-Sistema-de-Cadastro-de-Estagiarios/src/routes/privates.routes.jsx
+++ Aplicação/SCE-Sistema-de-Cadastro-de-Estagiarios/src/routes/privates.routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Estagiarios from '../pages/Estagiario'
+import CadastroEstagiarios from '../pages/CadastroEstagiario'
 
 const PrivateRoutes = () => {
     return (
@@ -8,6 +9,7 @@ const PrivateRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/estagiarios" element={<Estagiarios />} />
+                <Route path="/cadastroEstagiarios" element={<CadastroEstagiarios />} />
             </Routes>
         </BrowserRouter>
     )
